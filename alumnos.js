2941f5e5b6e6f92b4f9936fa3a623bab4879b8c3
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2025-12-01 22:37:05 -0300

    Actualizo cambios del proyecto

--- alumnos.js
+++ alumnos.js
@@ -60,25 +60,45 @@ function mostrarTabla(lista) {
 
     lista.forEach(a => {
         const tr = document.createElement("tr");
-        
-        const color = a.equipo === "morado" ? "#e5ccff" : "#ffffff";
 
+        // ------ CALCULAR ESTADO -------
+        const hoy = new Date();
+        const fechaVenc = new Date(a.fecha_vencimiento);
+
+        let claseEstado = "";
+
+        if (!a.fecha_vencimiento) {
+            claseEstado = "fila-sin-cuota";
+        } else if (fechaVenc < hoy) {
+            claseEstado = "fila-vencido";
+        } else {
+            const diferencia = Math.ceil((fechaVenc - hoy) / (1000 * 60 * 60 * 24));
+
+            if (diferencia <= 5) {
+                claseEstado = "fila-por-vencer";
+            } else {
+                claseEstado = "fila-al-dia";
+            }
+        }
+
+        // APLICAR CLASE A LA FILA
+        tr.classList.add(claseEstado);
+
+        // ------ CONTENIDO --------
         tr.innerHTML = `
-            <td style="background:${color}">${a.nombre}<br>${a.apellido}</td>
-            <td style="background:${color}">${a.dni}</td>
-            <td style="background:${color}">${a.nivel}</td>
-            <td style="background:${color}">${a.equipo}</td>
-            <td style="background:${color}">${formatearPlanes(a)}</td>
-            <td style="background:${color}">${formatFecha(a.fecha_vencimiento)}</td>
-
-            <td style="background:${color}">
-                <button class="btn-editar" onclick="editarAlumno(${a.id})">Editar</button>
-            </td>
+            <td>${a.nombre} ${a.apellido}</td>
+            <td>${a.dni}</td>
+            <td>${a.nivel}</td>
+            <td>${a.equipo}</td>
+            <td>${formatearPlanes(a)}</td>
+            <td>${formatFecha(a.fecha_vencimiento)}</td>
+            <td><button class="btn-edit" onclick="editarAlumno(${a.id})">Editar</button></td>
         `;
 
         tablaBody.appendChild(tr);
     });
 }
+
 
 /* ─────────────────────────────── */
 /* 🔹 Ir a editar */
