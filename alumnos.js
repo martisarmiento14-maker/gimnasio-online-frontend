b02be06d1e2940edd755d8c0ce0b0a9e73e4d11e
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2025-12-01 22:42:31 -0300

    Actualizo cambios del proyecto

--- alumnos.js
+++ alumnos.js
@@ -1,17 +1,11 @@
-const API_URL = "https://gimnasio-online-1.onrender.com";
+const API_URL = "https://gimnasio-online-frontend.onrender.com" || "http://localhost:3000";
 
 const tablaBody = document.getElementById("tablaAlumnosBody");
 const buscador = document.getElementById("buscador");
 
-// Cargar listado inicial
 document.addEventListener("DOMContentLoaded", cargarAlumnos);
-
-// Buscar por nombre o DNI
 buscador.addEventListener("input", cargarAlumnos);
 
-/* ─────────────────────────────── */
-/* 🔹 Formatear planes (como tu foto) */
-/* ─────────────────────────────── */
 function formatearPlanes(a) {
     let planes = [];
 
@@ -22,16 +16,10 @@ function formatearPlanes(a) {
     return planes.join(" + ");
 }
 
-/* ─────────────────────────────── */
-/* 🔹 Formato fecha DD/MM/YYYY */
-/* ─────────────────────────────── */
 function formatFecha(f) {
     return new Date(f).toLocaleDateString("es-AR");
 }
 
-/* ─────────────────────────────── */
-/* 🔹 Cargar alumnos */
-/* ─────────────────────────────── */
 async function cargarAlumnos() {
     try {
         const res = await fetch(`${API_URL}/alumnos`);
@@ -46,45 +34,17 @@ async function cargarAlumnos() {
         );
 
         mostrarTabla(filtrados);
-
     } catch (error) {
         console.log("Error cargando alumnos:", error);
     }
 }
 
-/* ─────────────────────────────── */
-/* 🔹 Mostrar tabla */
-/* ─────────────────────────────── */
 function mostrarTabla(lista) {
     tablaBody.innerHTML = "";
 
     lista.forEach(a => {
         const tr = document.createElement("tr");
 
-        // ------ CALCULAR ESTADO -------
-        const hoy = new Date();
-        const fechaVenc = new Date(a.fecha_vencimiento);
-
-        let claseEstado = "";
-
-        if (!a.fecha_vencimiento) {
-            claseEstado = "fila-sin-cuota";
-        } else if (fechaVenc < hoy) {
-            claseEstado = "fila-vencido";
-        } else {
-            const diferencia = Math.ceil((fechaVenc - hoy) / (1000 * 60 * 60 * 24));
-
-            if (diferencia <= 5) {
-                claseEstado = "fila-por-vencer";
-            } else {
-                claseEstado = "fila-al-dia";
-            }
-        }
-
-        // APLICAR CLASE A LA FILA
-        tr.classList.add(claseEstado);
-
-        // ------ CONTENIDO --------
         tr.innerHTML = `
             <td>${a.nombre} ${a.apellido}</td>
             <td>${a.dni}</td>
@@ -99,10 +59,6 @@ function mostrarTabla(lista) {
     });
 }
 
-
-/* ─────────────────────────────── */
-/* 🔹 Ir a editar */
-/* ─────────────────────────────── */
 function editarAlumno(id) {
     window.location.href = `form-alumno.html?editar=${id}`;
 }
